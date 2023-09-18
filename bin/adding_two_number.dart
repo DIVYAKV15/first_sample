cd6177013316c520e5413f8a0d8dec9e8aef4fc9
DIVYAKV15 <dhivya.kv1992@gmail.com>
2023-09-18 12:07:29 +0300

    inital commit

--- bin/adding_two_number.dart
+++ bin/adding_two_number.dart
@@ -7,5 +7,18 @@ void main() {
   var num1=int.parse(input1!);
   var num2=int.parse(input2!);
   print('the sum is ${num1+num2}');
-
+  String name='dhivya';
+  String name2="I 'm dhivya";
+  ///if you want to use the single quotes inside the value then use double quotes
+  String name3="I \"m Dhivya";
+  ///to use double quotes inside the value use \ infront of the double quotes
+  String name4='''i am dhivya
+  my address is    
+  ''';
+  ///to give multiple line in string use '''
+  print('i $name4');
+  print(name.length);
+  //to check the length of the value
+  print(name4.length);
+  //it will count including whitespace
 }
