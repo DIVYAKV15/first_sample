16dfd4f6cba593a58a64bf47a929dac164941249
DIVYAKV15 <dhivya.kv1992@gmail.com>
2023-09-20 09:09:02 +0300

    inital commit

--- bin/variable_exam/assignment1.dart
+++ bin/variable_exam/assignment1.dart
@@ -2,8 +2,8 @@ class Iphone
 {
   String? iphone_name;
   static String iphone_os='Apple_ios';
-  int? version;
-  String? capacity;
+  int? ios_version;
+  String? storage;
   double? display;
   
 
@@ -13,14 +13,14 @@ void main()
   Iphone ip=Iphone();
   print(" the model name is ${ip.iphone_name="iphone 14"}");
   print("the operating system is ${Iphone.iphone_os}");
-  print("the version of ios is ${ip.version=17}");
-  print("the capacity of iphone 14 is ${ip.capacity='128GB'}");
+  print("the version of ios is ${ip.ios_version=17}");
+  print("the capacity of iphone 14 is ${ip.storage='128GB'}");
   print("the display is ${ip.display=6.1}");
   print("******");
   Iphone ip1=Iphone();
   print(" the model name is ${ip1.iphone_name="iphone 14 plus"}");
   print("the operating system is ${Iphone.iphone_os}");
-  print("the version of ios is ${ip.version=17}");
-  print("the capacity of iphone 14 is ${ip.capacity='128GB'}");
+  print("the version of ios is ${ip.ios_version=17}");
+  print("the capacity of iphone 14 is ${ip.storage='128GB'}");
   print("the display is ${ip.display=6.8}");
 }
