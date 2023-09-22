c134d971ae10163ff542ccd627e857729e89e893
DIVYAKV15 <dhivya.kv1992@gmail.com>
2023-09-22 10:15:32 +0300

    inital commit

---
+++ bin/controlStatement/else_if_ladder.dart
@@ -0,0 +1,23 @@
+void main()
+{
+  String size='XXL';
+  if(size=='XS') {
+    print('YOUR SHIRT SIZE IS XS');
+  }
+  else if (size == 'S') {
+    print('YOUR SHIRT SIZE IS S');
+  }
+  else if (size == 'M') {
+    print('YOUR SHIRT SIZE IS M');
+  }
+  else if (size == 'L') {
+    print('YOUR SHIRT SIZE IS L');
+  }
+  else if (size == 'XL') {
+    print('YOUR SHIRT SIZE IS XL');
+  }
+else
+  {
+  print('YOUR SHIRT SIZE IS NOT AVAILABLE');
+  }
+}
---
+++ bin/controlStatement/switch_examples.dart
@@ -0,0 +1,23 @@
+void main()
+{
+  int size=9;
+  switch(size)
+      {
+    case 7:
+      {
+        print("your size is 7");
+        break;
+      }
+    case 8:
+      {
+        print("your size is 8");
+        break;
+      }
+    default:
+      {
+        print("your size is not there");
+      }
+  }
+}
+///here break keyword is not mandatory like java
+///but automatically it exit
