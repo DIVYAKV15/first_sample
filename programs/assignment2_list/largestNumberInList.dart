b48bedb01258a7a6c26ab9ba4f02f868f172f419
DIVYAKV15 <dhivya.kv1992@gmail.com>
2023-09-27 10:32:08 +0300

    inital commit

--- programs/assignment2_list/largestNumberInList.dart
+++ programs/assignment2_list/largestNumberInList.dart
@@ -1,13 +1,10 @@
-void main()
-{
-    List<int> list=[1,-3,7,9,0,-6,4,2,0,10,-8,5];
-    var large_value=list[0];
-    for(var i=0;i<list.length;i++)
-      {
-        if(list[i]>large_value)
-          {
-            large_value=list[i];
-          }
-      }
-    print('largest number in list is $large_value');
+void main() {
+  List<int> list = [1, -3, 7, 9, 0, -6, 4, 2, 0, 10, -8, 5];
+  var large_value = list[0];
+  for (var i = 0; i < list.length; i++) {
+    if (list[i] > large_value) {
+      large_value = list[i];
+    }
+  }
+  print('largest number in list is $large_value');
 }
