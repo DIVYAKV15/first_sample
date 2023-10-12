f524ddb5252a5f7ff848a13d01e934da7fa08bba
DIVYAKV15 <dhivya.kv1992@gmail.com>
2023-10-12 21:45:54 +0300

    primenumber

--- programs/PrimeNumber.dart
+++ programs/PrimeNumber.dart
@@ -4,7 +4,7 @@ void main() {
   print("enter the value to check prime number or not");
   int? value = int.parse(stdin.readLineSync()!);
   bool isPrime = true;
-  for (var i = 2; i <= value; i++) {
+  for (var i = 2; i < value; i++) {
     if (value % i == 0) {
       isPrime = false;
       break;
